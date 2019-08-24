5b1652d71c32b9804d176e91befef33c591a6fe8
fardinkhan <fardinkhan@uvic.ca>
2019-08-24 09:06:44 +0600

    added the best sol commented

--- prob_1_find_it.py
+++ prob_1_find_it.py
@@ -3,6 +3,7 @@ Given an array, find the int that appears an odd number of times.
 There will always be only one integer that appears an odd number of times.
 """
 
+#My solution, lol not Pythonic at all fml
 def find_it(seq):
     l = len(seq)
     count = 0
@@ -17,3 +18,12 @@ def find_it(seq):
 print(find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]))
 
 #correct output = 5
+
+"""
+The best solution someone else submitted:
+
+def find_it(seq):
+    for i in seq:
+        if seq.count(i)%2!=0:
+            return i
+"""
